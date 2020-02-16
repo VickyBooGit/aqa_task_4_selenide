2853ab836d770b23101bc6bbc957eca033eddb57
Victoria Bukina <victoria.bukina@gmail.com>
2020-02-16 20:45:35 +0300

    Changing permission of gradlew

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -1,31 +1,20 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CardDelivery {
 
     private WebDriver driver;
-
-
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
-    }
+//    @BeforeAll
+//    static void setUpAll() {
+//        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+//    }
 
     @Test
     void shouldDeliveryCardByContactData() {
